aac94b1e9ce471fb1470b7930fc8543a460dfab6
AlexeyDmt <110967279+AlexeyDmt@users.noreply.github.com>
2024-08-11 18:48:23 +0300

    Add files via upload

---
+++ precache-manifest.03fcbb798c2e23a8bc332cdbdcf4e860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803a87301fb95411e5e3f2114d231547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e8a72dcc37e0583c71d",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "49d53756381de06665bb",
+    "url": "/static/js/2.ba7e1342.chunk.js"
+  },
+  {
+    "revision": "5e8a72dcc37e0583c71d",
+    "url": "/static/js/main.d30aa9ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
